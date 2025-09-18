b76f78d4c5d7e96b66ac41d04ee29e55e58c74bb
aljml-cubes <mohammed.aljamal@cubesplatform.com>
2025-09-18 17:03:43 +0300

    feat: add login page and update routing

--- src/i18n/ar.ts
+++ src/i18n/ar.ts
@@ -3,4 +3,10 @@ export default {
   light: "فاتح",
   arabic: "العربية",
   english: "الإنجليزية",
+  enterUserName: "أدخل اسم المستخدم",
+  enterPass: "أدخل كلمة المرور",
+  userName: "اسم المستخدم",
+  password: "كلمة المرور",
+  login: "تسجيل الدخول",
+  invalidLogin: "اسم المستخدم أو كلمة المرور غير صحيحة",
 };
